193a76fbd5e689ccf612707e79f4d74708b179f2
Reagan Russell <nitrodragon@users.noreply.github.com>
2017-10-23 23:08:11 -0700

    Declutter build.bat
    Removing statements used for debugging only.

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
-C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o src\main.o src\main.c
-C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb src\main.o
+:: C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o src\main.o src\main.c
+:: C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb src\main.o
+:: pause
+
+C:\gbdk\bin\lcc -c -o src\main.o src\main.c
+C:\gbdk\bin\lcc -o main.gb src\main.o
 pause
