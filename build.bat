2b5002ed6e1dee0ae99c862fc6a94f84b1630dea
Reagan Russell <nitrodragon@users.noreply.github.com>
2017-10-23 23:01:16 -0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
-C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o src\main.o src\main.c
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb src\main.o
 pause
