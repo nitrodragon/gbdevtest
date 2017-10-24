a267f3a346fe54cdee03ff4efeda5e5cf5b967f5
nitrodragon <nitrodragon@users.noreply.github.com>
2017-10-23 23:29:59 -0700

    Minimal branch
    And the best `.bat` file I've ever written.

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 :: C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o src\main.o src\main.c
 :: C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb src\main.o
 :: pause
-
-C:\gbdk\bin\lcc -c -o src\main.o src\main.c
-C:\gbdk\bin\lcc -o main.gb src\main.o
+if not exist "C:\gbdk\out" mkdir C:\gbdk\out
+C:\gbdk\bin\lcc -c -o out\main.o src\main.c
+C:\gbdk\bin\lcc -o main.gb out\main.o
+@rd /S /Q "C:\gbdk\out"
 pause
